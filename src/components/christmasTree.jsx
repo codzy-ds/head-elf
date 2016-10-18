dac17ad956eecc575b91bf5a37240f1e0da077fc
mitch <michel.bernier@technologist.com>
2016-10-18 01:11:35 -0400

    Fir modification.

--- src/components/christmasTree.jsx
+++ src/components/christmasTree.jsx
@@ -3,8 +3,7 @@ import './styles.css'
 
 class ChristmasTree extends React.Component {
   render() {
-  return (
-    <div className="ctree">
+    return (<div className="ctree">
       <div className="trunk"></div>
       <div className="branches bottom"><div className="band"></div></div>
       <div className="branches middle"><div className="band"></div></div>
@@ -17,7 +16,21 @@ class ChristmasTree extends React.Component {
       <div className="boule-argent1"></div>
       <div className="boule-argent2"></div>
     </div>
-  )}
+)
+  }
 }
+
+/* <div className="fir">
+  <div className="fir__item"></div>
+  <div className="fir__item"></div>
+  <div className="fir__item"></div>
+
+  <div className="fir__log"></div>
+
+  <div className="orbs orbs-1"></div>
+  <div className="orbs orbs-2"></div>
+  <div className="orbs orbs-3"></div>
+  <div className="orbs orbs-4"></div>
+</div> */
 
 export default ChristmasTree
