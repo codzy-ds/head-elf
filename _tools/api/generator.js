3d801cf7aca2474c978fae08780b0643e361e4a8
Michel Bernier <michel.bernier@ticketmaster.com>
2016-10-31 14:30:44 -0400

    Remove christmas tree if screen too small (like on a cell phone) Add mocked apis for personalities and tags. To finish the search form.

--- _tools/api/generator.js
+++ _tools/api/generator.js
@@ -16,10 +16,29 @@ const getTricks = () => {
   }
 }
 
+const getTags = () => {
+  return {
+    tags: _.times(500, () => {
+      return faker.lorem.word()
+    })
+  }
+}
+
+const getPersonalities = () => {
+  return {
+    personalities: _.map(['gluton', 'tidy', 'messy', 'ingenious', 'acrobat', 'arstistic'], (personality) => {
+      return {
+        type: personality,
+        tags: _.times(3, () => { return faker.lorem.word()})
+      }
+    })
+  }
+}
+
 const getIntro = () => {
   return {
     info: {info: "Si vous voulez attraper un lutins, graillez-vous d'une batch de guimauves"}
   }
 }
 
-module.exports = () => Object.assign({}, getTricks(), getIntro())
+module.exports = () => Object.assign({}, getTricks(), getIntro(), getTags(), getPersonalities())
