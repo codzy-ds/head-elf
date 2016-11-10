db28da55675f0331ac5a7ccda773b0afa4db6848
Michel Bernier <michel.bernier@ticketmaster.com>
2016-11-10 13:14:00 -0500

    Add a home button

--- src/containers/header.jsx
+++ src/containers/header.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Header = () => {
   return (<div className="App-header">
-    <ul id="bauble-container">
-      <li>
-        <div className="bauble red-bauble"></div>
-      </li>
-      <li>
-        <div className="bauble blue-bauble"></div>
-      </li>
-      <li>
-        <div className="bauble yellow-bauble"></div>
-      </li>
-      <li>
-        <div className="bauble green-bauble"></div>
-      </li>
-    </ul>
-  </div>)
+  <ul id="bauble-container">
+    <li>
+      <div className="bauble red-bauble"></div>
+    </li>
+    <li>
+      <div className="bauble blue-bauble"></div>
+    </li>
+    <li>
+      <div className="bauble yellow-bauble"></div>
+    </li>
+    <li>
+      <div className="bauble green-bauble"></div>
+    </li>
+  </ul>
+  <div id="ctrlBtns"><Link to='/' id='go-home-btn'>home</Link></div>
+</div>)
 }
 
 export default Header
