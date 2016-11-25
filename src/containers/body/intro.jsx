c778f0bb4e296c762c070d7b1cd420f4d92d76d4
codzy-ds <codzy@techie.com>
2016-11-25 06:55:30 -0500

    Put static html in info.

--- src/containers/body/intro.jsx
+++ src/containers/body/intro.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import infoStore from './infoStore'
 
 const Intro = () => {
   return(
     <section>
-      <h4></h4>
+      <h4>Les Lutins de no&euml;l</h4>
+      <p>Les lutins de noël envahissent les maison des québecois depuis quelques années.</p>
+      <p>Pour attrapper un lutin, il s'agit de lui tendre un piège.</p>
     </section>)
 }
 export default Intro
