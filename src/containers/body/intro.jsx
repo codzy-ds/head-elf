77f5813a7673d87caee952fda288bccf1fc722c0
codzy-ds <codzy@techie.com>
2016-11-25 01:15:50 -0500

    add the about section and infos sections.

--- src/containers/body/intro.jsx
+++ src/containers/body/intro.jsx
@@ -1,28 +1,10 @@
 import React from 'react'
 import infoStore from './infoStore'
 
-class Intro extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = infoStore.getModel()
-  }
-
-  componentDidMount() {
-    this.unsubscribe = infoStore.listen(this.onStatusChange.bind(this));
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe()
-  }
-
-  onStatusChange() {
-    this.setState(infoStore.getModel())
-  }
-
-  render() {
-    let info = this.state
-    return (<p>{info.info}</p>)
-  }
+const Intro = () => {
+  return(
+    <section>
+      <h4></h4>
+    </section>)
 }
-
 export default Intro
