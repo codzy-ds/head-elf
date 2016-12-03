d5898d907da98957e6e727c5d4d8527919a63e38
codzy-ds <codzy@techie.com>
2016-12-03 10:52:15 -0500

    Typos before the beta release

--- src/containers/about.jsx
+++ src/containers/about.jsx
@@ -4,13 +4,25 @@ const About = () => {
   return(
     <section>
       <h2>Lutin</h2>
-      <h3>v1.0</h3>
+      <h3>v1.0 beta</h3>
       <p>
         L'application Lutin vous est apportée par Hamster labs.<br />
         Cette application a été fait dans un but d'apprentissage de plusieurs technologies.<br />
         Amusez-vous à l'utiliser, entrez vos mauvais coups, cherchez des mauvais coups.<br />
         Cette application viens sans aucune garantie et est pour fins récréationelles seulement.<br />
       </p>
+      <p>
+        N'oubliez jamais que l'important lorsqu'on a des lutins de Noël à la maison, c'est de s'amuser!
+        Chez Hamsterlabs, on s'amuse comme des fous!
+      </p>
+      <h3>Sources</h3>
+      <ul>
+        <li><a href='http://www.coupdepouce.com/mamans/loisirs-et-jeux/article/lutins-de-noel-25-idees-de-tours-trouvees-sur-pinterest'>25 coups de lutins - coup de pouce</a></li>
+        <li><a href='https://www.pouponsetcie.ca/blogue/la-chasse-aux-lutins/25-idees-de-tours-de-lutin-de-noel/'>35 coups de lutins - Poupons et Cie</a></li>
+        <li><a href='http://lutins.dompteurs.com/'>Calentrier de l'avent de lutin</a></li>
+        <li><a href='http://veroniquecloutier.com/famille/noel-10-idees-de-tours-de-lutin'>10 tours de lutins de véronique cloutier</a></li>
+        <li><a href='http://www.canalvie.com/famille/vie-de-famille/idees-tours-lutin-de-noel-1.1425453'>25 idées pour votre lutin - canal vie</a></li>
+      </ul>
     </section>
   )
 }
--- src/containers/body/intro.jsx
+++ src/containers/body/intro.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Intro = () => {
   return(
     <section>
-      <h4>Les Lutins de no&euml;l</h4>
+      <h4>Les Lutins de No&euml;l</h4>
       <p>Les lutins de noël envahissent les maison des québecois depuis quelques années.</p>
       <p>Quelques fois, les lutins peuvent manquer d'inspiration. Utilisez cette
         application pour vous redonner de l'inspiration. Vous pouvez rechercher des tours,
